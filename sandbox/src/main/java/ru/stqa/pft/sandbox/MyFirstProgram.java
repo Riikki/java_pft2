5eb15006e6088394b606b85ea2f645cf48119e96
Anastasiya Tsitova <riikkimarley@gmail.com>
2017-07-27 17:23:39 +0300

    Add some functions

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,8 +2,28 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 
-public static void main(String[] args)
-{
-  System.out.println("Hello, world!");
+	public static void main(String[] args) {
+		hello("Anastasiya");
+
+		double l = 5;
+		System.out.println(area(l));
+
+		double a = 4;
+		double b = 4;
+		System.out.println(area(a,b));
+	}
+
+	public static void hello(String somebody) {
+		System.out.println("Hello, " + somebody + "!");
+	}
+
+	public static double area(double l) {
+		return l*l;
+	}
+		public static double area(double a, double b) {
+		return a*b;
 }
-}
+	}
+
+
+
