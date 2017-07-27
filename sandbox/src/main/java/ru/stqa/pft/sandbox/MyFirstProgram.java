dc7a78de907897c690d6a0fbc04590f0445c9a20
Anastasiya Tsitova <riikkimarley@gmail.com>
2017-07-27 18:13:23 +0300

    add last lesson

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,27 +1,28 @@
 package ru.stqa.pft.sandbox;
 
+import java.awt.*;
+
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
 		hello("Anastasiya");
 
-		double l = 5;
-		System.out.println(area(l));
+		Square s = new Square(5);
+		System.out.println(area(s));
 
-		double a = 4;
-		double b = 4;
-		System.out.println(area(a,b));
+		Rectangle r = new Rectangle(4,6);
+			System.out.println(area(r));
 	}
 
 	public static void hello(String somebody) {
 		System.out.println("Hello, " + somebody + "!");
 	}
 
-	public static double area(double l) {
-		return l*l;
+	public static double area(Square s) {
+		return s.l*s.l;
 	}
-		public static double area(double a, double b) {
-		return a*b;
+		public static double area(Rectangle r) {
+		return r.a*r.b;
 }
 	}
 
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,14 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by user on 7/27/2017.
+ */
+public class Square {
+
+	public double l;
+
+	public Square(double l){
+		this.l = l;
+	}
+
+}
