d3b8cb3bb9d523a1872cf5c57a1ef997775f3ee0
Anastasiya Tsitova <riikkimarley@gmail.com>
2017-07-28 14:16:45 +0300

    Complete lesson1

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,6 +1,6 @@
 package ru.stqa.pft.sandbox;
 
-import java.awt.*;
+import static java.lang.Math.sqrt;
 
 public class MyFirstProgram {
 
@@ -12,7 +12,19 @@ public class MyFirstProgram {
 
 		Rectangle r = new Rectangle(4,6);
 			System.out.println(area(r));
+
+		Point p1 = new Point(1,1);
+		Point p2 = new Point(5,4);
+		distance(p1,p2);
+
 	}
+
+	public static void distance(Point p1, Point p2){
+		System.out.println(Math.pow((p2.x - p1.x),2));
+		System.out.println(Math.pow((p2.y - p1.y),2));
+		System.out.println(sqrt(Math.pow((p2.x - p1.x),2)+Math.pow((p2.y - p1.y),2)));
+	}
+
 
 	public static void hello(String somebody) {
 		System.out.println("Hello, " + somebody + "!");
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by user on 7/28/2017.
+ */
+public class Point{
+
+	public double x;
+	public double y;
+
+	public Point(double x, double y) {
+		this.x=x;
+		this.y= y;
+	}
+
+}
